f7a32f5b8d463c2cf9b156f4501fad8b13f8c180
Erik Nyquist <eknyquist@gmail.com>
2017-05-06 20:20:50 -0700

    Initial commit

---
+++ README.rst
@@ -0,0 +1,49 @@
+Brainfuck compiler
+==================
+
+Compiles `Brainfuck <https://en.wikipedia.org/wiki/Brainfuck>`_ source code into
+executable format.
+
+(Actually, more like a brainfuck-to-C translator that invokes GCC to get an
+executable)
+
+Two sample brainfuck programs, ``hello_world.b`` (prints "Hello world!" to the
+terminal), and ``sierpinski.b`` (prints a pretty ASCII triangle to the
+terminal) are provided in this repository.
+
+Install
+=======
+
+::
+
+    > make
+    > sudo make install
+
+Now, you have two new programs installed:
+
+    * ``btoc`` (reads a brainfuck source file, prints C to stdout)
+    * ``bcc`` (reads a brainfuck source file, and creates an executable by
+      running ``btoc`` and piping it to ``gcc``)
+
+Compile a brainfuck program
+===========================
+
+If you have GCC and a C standard lib. installed, you can use ``bcc`` to compile
+brainfuck programs directly into executable format. Here is an example, Using
+``hello_world.b``
+
+::
+
+    > bcc hello_world.b -o hello_world
+    > ./hello_world
+
+    Hello World!
+
+    >
+
+Or, if (for some reason) you want to see the C code, you can just run ``btoc``
+on the brainfuck program yourself
+
+::
+
+    > btoc hello_world.b > hello_world.c
---
+++ btoc.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+
+char prev;
+unsigned int indentation;
+unsigned int seq;
+
+const char *head = "#include <stdio.h>\n\n"
+                   "static char array[30000] = {0};\n"
+                   "static char *p = array;\n\n"
+                   "int main (void)\n{";
+
+void indent(void)
+{
+    int i;
+    for (i = 0; i < indentation; ++i) printf(" ");
+}
+
+void indent_line (const char *code)
+{
+    indent();
+    printf("%s\n", code);
+}
+
+void print_sequence (void)
+{
+    const char *snip;
+
+    switch (prev) {
+        case '>':
+            snip = "p +";
+        break;
+        case '<':
+            snip = "p -";
+        break;
+        case '+':
+            snip = "*p +";
+        break;
+        case '-':
+            snip = "*p -";
+        break;
+    }
+
+    indent();
+    printf("%s= %d;\n", snip, seq);
+}
+int main (int argc, char *argv[])
+{
+    unsigned int depth;
+    FILE *fp;
+    char c;
+
+    if (argc == 2) {
+        if ((fp = fopen(argv[1], "r")) == NULL) {
+            printf("Error: can't read file %s\n", argv[1]);
+            return 1;
+        }
+    } else if (argc == 1) {
+        fp = stdin;
+    } else {
+        printf("Usage: %s <brainfuck_source_file>\n", argv[0]);
+        return 1;
+    }
+
+    depth = 0;
+    seq = 0;
+    indentation = 4;
+    printf("%s\n", head);
+
+    while((c = fgetc(fp)) != EOF) {
+        if ((c == '>' || c == '<' || c == '+' || c == '-')) {
+            if (seq > 0 && c != prev) {
+                print_sequence();
+                seq = 0;
+            }
+
+            ++seq;
+        } else if (seq > 0) {
+            print_sequence();
+            seq = 0;
+        }
+
+        switch (c) {
+            case '.':
+                indent_line("putchar(*p);");
+            break;
+            case ',':
+                indent_line("*p = getchar();");
+            break;
+            case '[':
+                printf("\n");
+                indent_line("while (*p) {");
+                indentation += 4;
+                ++depth;
+            break;
+            case ']':
+                if (depth == 0) {
+                    fprintf(stderr, "\nError: unmatched ']' in %s\n", argv[1]);
+                    return 1;
+                }
+
+                indentation -= 4;
+                indent_line("}\n");
+                --depth;
+            break;
+        }
+
+        prev = c;
+    }
+
+    if (depth != 0) {
+        fprintf(stderr, "\nError: unmatched '[' in %s\n", argv[1]);
+        return 1;
+    }
+
+    indent_line("return 0;\n}\n");
+    fclose(fp);
+    return 0;
+}
